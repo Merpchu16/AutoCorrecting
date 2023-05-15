2ce0783ba3d9efb22f50d1b88ad3bf21ac8315f9
Merpchu16 <72161931+Merpchu16@users.noreply.github.com>
2023-05-15 14:49:26 +0000

    work pt 2

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # AutoCorrecting
 
-pip install pyspellchecker
+Hello! Welcome to my autocorrector in python.
+To begin, please install these two libraries below
+
+# pip install pyspellchecker
+# pip install python-Levenshtein
+
+Run the program and in the terminal you will see:
+# Enter a sentence: 
+There you will enter your desired sentance, be it mispelled or even correct type it in!
+Next, you'll enter your sentance and the following will pop up
+## Corrected sentence:
+## Number of corrected words:
+## Total edit distance: 
+ Here, my program gives you the corrected sentance
+ Then, how many words it corrected
+ and lastly the total of edits it had to make inbetween.
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from spellchecker import SpellChecker
+# from spellchecker import SpellChecker
 
-spell = SpellChecker()
+# spell = SpellChecker()
 
-# find those words that may be misspelled
-misspelled = spell.unknown(['something', 'is', 'hapenning', 'here'])
+# # find those words that may be misspelled
+# misspelled = spell.unknown(['something', 'is', 'hapenning', 'here'])
 
-for word in misspelled:
-    # Get the one `most likely` answer
-    print(spell.correction(word))
+# for word in misspelled:
+#     # Get the one `most likely` answer
+#     print(spell.correction(word))
 
-    # Get a list of `likely` options
-    print(spell.candidates(word))
+#     # Get a list of `likely` options
+#     print(spell.candidates(word))
 
 # from spellchecker import 
 
@@ -29,3 +29,47 @@ for word in misspelled:
 #         corrected_word = spell.correction(word)
 #         corrected_words.append(corrected_word)
 #     return ' '.join(corrected_words)
+
+
+from spellchecker import SpellChecker
+from Levenshtein import distance
+
+# create a SpellChecker object
+spell = SpellChecker()
+
+# ask for the user's possible misspelled sentance
+sentence = input("Enter a sentence: ")
+
+# split the sentence
+words = sentence.split()
+
+# variables to track the number of corrected words and total edit distance
+num_corrected = 0
+total_edit_distance = 0
+
+# loop through each word in the sentence
+for i, word in enumerate(words):
+    # find the corrected spelling of the word
+    corrected_word = spell.correction(word)
+    
+    # calculate the edit distance between the original word and the corrected word
+    edit_distance = distance(word, corrected_word)
+    
+    # update the total edit distance
+    total_edit_distance += edit_distance
+    
+    # check if the word was corrected
+    if word != corrected_word:
+        # update the number of corrected words
+        num_corrected += 1
+        
+        # replace the original word with the corrected word
+        words[i] = corrected_word
+
+# join the corrected words back into a sentence
+corrected_sentence = " ".join(words)
+
+# output the corrected sentence and summary statistics
+print("Is this what you meant?: ", corrected_sentence)
+print("Number of corrected words: ", num_corrected)
+print("Total edit distance: ", total_edit_distance)
